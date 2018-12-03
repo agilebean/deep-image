3c9943d6029a7deecb945f1514ffc60959d3375d
Chaehan So <chaehan.so@gmail.com>
2018-12-03 22:25:35 +0900

    Refactoring to generic code (models, html_content_list). added more models (total 10)

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pdb
 from time import time
 from flask import Flask, render_template, request
 
-from keras.applications import resnet50, inception_v3, mobilenet, xception, vgg16, vgg19
+from keras.applications import vgg16, vgg19, resnet50, inception_v3, mobilenet, xception, densenet, inception_resnet_v2,  nasnet
 from keras.preprocessing import image
 
 import numpy as np
@@ -30,29 +30,83 @@ import mymodels
 # create Flask app
 app = Flask(__name__)
 
-def predict_image(image_input, model) -> str:
-    tic = time()
-    print("predict_image() entered")
-
-    if (model.name == 'mobilenet_1.00_224'):
-        #model.name = 'mobilenet'
-        model_preprocess_input = mobilenet.preprocess_input
-        model_decode_predictions = mobilenet.decode_predictions
-    elif (model.name == 'resnet50'):
-        model_preprocess_input = resnet50.preprocess_input
-        model_decode_predictions = resnet50.decode_predictions
-    elif (model.name == 'inception_v3'):
-        model_preprocess_input = inception_v3.preprocess_input
-        model_decode_predictions = inception_v3.decode_predictions
-    elif (model.name == 'xception'):
-        model_preprocess_input = xception.preprocess_input
-        model_decode_predictions = xception.decode_predictions
-    elif (model.name == 'vgg16'):
+def get_model_data(model):
+    print('***** model name ' + model.name)
+    if (model.name == 'vgg16'):
         model_preprocess_input = vgg16.preprocess_input
         model_decode_predictions = vgg16.decode_predictions
+        image_size = 224
+        top1_acc = '71.5'
+        top5_acc = '90.1'
     elif (model.name == 'vgg19'):
         model_preprocess_input = vgg19.preprocess_input
         model_decode_predictions = vgg19.decode_predictions
+        image_size = 224
+        top1_acc = '72.7'
+        top5_acc = '91.0'
+    elif (model.name == 'resnet50'):
+        model_preprocess_input = resnet50.preprocess_input
+        model_decode_predictions = resnet50.decode_predictions
+        image_size = 224
+        top1_acc = '75.9'
+        top5_acc = '92.9'
+    elif (model.name == 'inception_v3'):
+        model_preprocess_input = inception_v3.preprocess_input
+        model_decode_predictions = inception_v3.decode_predictions
+        image_size = 299
+        top1_acc = '78.8'
+        top5_acc = '94.4'
+    elif (model.name == 'xception'):
+        model_preprocess_input = xception.preprocess_input
+        model_decode_predictions = xception.decode_predictions
+        image_size = 299
+        top1_acc = '79.0'
+        top5_acc = '94.5'
+    elif (model.name == 'mobilenet_1.00_224'):
+        #model.name = 'mobilenet'
+        model_preprocess_input = mobilenet.preprocess_input
+        model_decode_predictions = mobilenet.decode_predictions
+        image_size = 224
+        top1_acc = '70.6' # Source: https://arxiv.org/pdf/1704.04861.pdf
+        top5_acc = '87.1'
+    elif (model.name == 'densenet201'):
+        model_preprocess_input = densenet.preprocess_input
+        model_decode_predictions = densenet.decode_predictions
+        image_size = 224
+        top1_acc = '77.0'
+        top5_acc = '93.3'
+    elif (model.name == 'NASNet'):
+        model_preprocess_input = inception_resnet_v2.preprocess_input
+        model_decode_predictions = inception_resnet_v2.decode_predictions
+        image_size = 224
+        top1_acc = '74.0' # Source: https://github.com/tensorflow/models/tree/master/research/slim/nets/nasnet
+        top5_acc = '91.6' # Source: dito
+    elif (model.name == 'inception_resnet_v2'):
+        model_preprocess_input = inception_resnet_v2.preprocess_input
+        model_decode_predictions = inception_resnet_v2.decode_predictions
+        image_size = 299
+        top1_acc = '80.1' # 80.4
+        top5_acc = '95.1' # 95.3
+    elif (model.name == 'mobilenetv2_1.00_224'):
+        model_preprocess_input = inception_resnet_v2.preprocess_input
+        model_decode_predictions = inception_resnet_v2.decode_predictions
+        image_size = 224
+        top1_acc = '71.8' #Source: https://github.com/tensorflow/models/tree/master/research/slim/nets/mobilenet
+        top5_acc = '91.0' # Source: dito
+    
+    return (top1_acc, top5_acc, model_preprocess_input, model_decode_predictions, image_size)
+
+def predict_image(image_path, model) -> str:
+    tic = time()
+    model_name = model.name
+    print("*** entered predict_image() by " + model_name)
+    
+    ( top1_acc, top5_acc,
+    model_preprocess_input, 
+    model_decode_predictions, 
+    image_size ) = get_model_data(model)
+
+    image_input = image.load_img(image_path, target_size = (image_size, image_size))
 
     # read image input
     img = image.img_to_array(image_input)
@@ -60,88 +114,69 @@ def predict_image(image_input, model) -> str:
     img = np.expand_dims(img, axis=0)
     # prepare the image for the models
 
-    if (model.name == 'resnet50'):
+    if (model_name == 'resnet50'):
         img = model_preprocess_input(img, mode='caffe')
     else:
         img = model_preprocess_input(img)
-
     print("image preprocessed")
-    #pdb.set_trace()
 
     # predict the probability across all output classes
     predictions = model.predict(img)
-
     print("image predicted")
-    #pdb.set_trace()
 
     # convert the probabilities to class labels
     label = model_decode_predictions(predictions, top=1)
-
     print("predictions decoded")
 
     # retrieve the most likely result, e.g. highest probability
     label = label[0][0]
-    # print the classification
-    #classification = '%s predicts: %s (%.2f%%)' % (model.name, label[1], label[2]*100)
 
     toc = time()
-
     processing_time = toc-tic
-    print("Image classified by %s in >> %.2f s" % (model.name, processing_time))
+    print("Image classified by %s in >> %.2f s" % (model_name, processing_time))
 
-    classification = ( model.name, processing_time, label[2]*100, label[1] )
+    classification = ( model_name, top1_acc, top5_acc, processing_time, label[2]*100, label[1] )
     print("image classified >> " + str(classification))
 
-    return (classification)
+    return classification
 
 
-def get_file_path_and_save(request):
+def upload_image(request):
     # Get the file from post request
     f = request.files['file']
 
     # Save the file to ./uploads
     basepath = os.path.dirname(__file__)
-    file_path = os.path.join(
+    image_path = os.path.join(
         basepath, 'uploads', secure_filename(f.filename))
-    f.save(file_path)
-    return file_path
+    f.save(image_path)
+    return image_path
+
 
 @app.route('/', methods = ['GET'])
 def show_index():
     print('entered homepage')
     return render_template('index.html')
 
+
 @app.route('/predict', methods = ['GET', 'POST'])
 def generate_predictions():
     print('entered predict image')
 
-    def row_prediction(image_input, model):
-        model_name, ptime, confidence, label = predict_image(image_input, model)
-        return '<tr><td> %s </td><td> %.2fs </td><td> %.2f%% </td><td> %s </td></tr>' % (model_name, ptime, confidence, label)
-
     if request.method == 'POST':
         tic = time()
-        file_path = get_file_path_and_save(request)
-        img_224 = image.load_img(file_path, target_size = (224,224))
-        img_299 = image.load_img(file_path, target_size = (299,299))
+        image_path = upload_image(request)
 
         result = '<table style="width:70%">'
-        result += '<caption>Image Classifications for %s </caption>' % (os.path.basename(file_path))
-        result += '<tr><th>Machine Learning Model</th><th>Time</th><th>Confidence</th><th>Classification</th></tr>'
+        result += '<caption>Image Classifications for %s </caption>' % (os.path.basename(image_path))
+        result += '<tr><th>Machine Learning Model</th><th>Top1-Acc</th><th>Top5-Acc</th><th>Time</th><th>Confidence</th><th>Classification</th></tr>'
 
-
-        result += row_prediction(img_224, mymodels.model_vgg16)
+        content_list = [ predict_image(image_path, model) for model in mymodels.models ]
         
-        result += row_prediction(img_224, mymodels.model_vgg19)
-
-        result += row_prediction(img_224, mymodels.model_resnet50)
-
-        result += row_prediction(img_299, mymodels.model_inception_v3)
-
-        result += row_prediction(img_299, mymodels.model_xception)
+        html_content_list = [ '<tr><td> %s </td><td>%s%%</td><td>%s%%</td><td> %.2fs </td><td> %.2f%% </td><td> %s </td></tr>' % (model_name, top1_acc, top5_acc, ptime, confidence, label)
+            for (model_name, top1_acc, top5_acc, ptime, confidence, label) in content_list ]
         
-        result += row_prediction(img_224, mymodels.model_mobilenet)
-        
+        result += '\n'.join(html_content_list)
 
         result += '</table>'
 
@@ -155,9 +190,10 @@ if (__name__ == '__main__'):
     print('* Loading Keras models and starting Flask server...')
     print('.....')
     # get wsgi server to replace flask app.run()
-    from gevent.pywsgi import WSGIServer
-    web_server = WSGIServer(('', 5000), app)
-    web_server.serve_forever()
+    #from gevent.pywsgi import WSGIServer
+    #web_server = WSGIServer(('', 5000), app)
+    #web_server.serve_forever()
+    app.run(host='0.0.0.0')
 
     print('Success! Server available at http://127.0.0.1:5000')
 
--- mymodels.py
+++ mymodels.py
@@ -10,49 +10,24 @@ ticTotal = time()
 def load_keras_model(model_label):
     
     tic = time()
-    model = load_model(os.path.join(current_dir, 'models', model_label, '.h5'))
+    model = load_model(os.path.join(current_dir, 'models', model_label + '.h5'))
     toc = time()
     print("%s model loaded in %.2fs" % (model_label, toc-tic))
     return model
 
-ticTotal = time()
+model_labels = [ 'VGG16',
+                 'VGG19',
+                 'ResNet50',
+                 'InceptionV3',
+                 'Xception',
+                 'MobileNet',
+                 'MobileNetV2',
+                 'NASNetMobile',
+                 'DenseNet201',
+                 'InceptionResNetV2'
+               ]
 
-model_mobilenet = load_model(os.path.join(current_dir, 'models/MobileNet.h5'))
-toc = time()
-#model_mobilenet = MobileNet(weights = 'imagenet', include_top = True)
-print("MobileNet model loaded in %.2fs" % (toc-tic))
-
-
-tic = time()
-model_resnet50 = load_model(os.path.join(current_dir, 'models/ResNet50.h5'))
-toc = time()
-#model_resnet50 = ResNet50(weights = 'caffe', include_top = True)
-print("ResNet50 model loaded in %.2fs" % (toc-tic))
-
-
-tic = time()
-model_inception_v3 = load_model(os.path.join(current_dir, 'models/InceptionV3.h5'))
-toc = time()
-#model_inception_v3 = InceptionV3(weights = 'imagenet', include_top = True)
-print("InceptionV3 model loaded in %.2fs" % (toc-tic))
-
-
-tic = time()
-model_xception = load_model(os.path.join(current_dir, 'models/Xception.h5'))
-toc = time()
-#model_inception_v3 = InceptionV3(weights = 'imagenet', include_top = True)
-print("Xception model loaded in %.2fs" % (toc-tic))
-
-
-tic = time()
-model_vgg16 = load_model(os.path.join(current_dir, 'models/VGG16.h5'))
-toc = time()
-print("VGG16 model loaded in %.2fs" % (toc-tic))
-
-tic = time()
-model_vgg19 = load_model(os.path.join(current_dir, 'models/VGG19.h5'))
-toc = time()
-print("VGG19 model loaded in %.2fs" % (toc-tic))
+models = [ load_keras_model(model_label) for model_label in model_labels ]
 
 
 tocTotal = time()
