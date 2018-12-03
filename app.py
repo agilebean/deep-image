d0992c6fe2f0319d01d2f4bf5fbaf239f2601f29
Chaehan So <chaehan.so@gmail.com>
2018-12-03 22:20:56 +0900

    output in html table by row_prediction(). added more models (total 6). tried  pdb

--- app.py
+++ app.py
@@ -1,77 +1,99 @@
+from keras import backend
+import tensorflow as tf
+
+session = tf.Session(config=tf.ConfigProto(
+    intra_op_parallelism_threads=1,
+    inter_op_parallelism_threads=1,
+    allow_soft_placement=True
+    ))
+
+backend.set_session(session)
+
+import pdb
+from time import time
 from flask import Flask, render_template, request
 
-import keras
-from keras.models import load_model
-from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-from keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
-from keras.applications.mobilenet import MobileNet, preprocess_input, decode_predictions
-
-
+from keras.applications import resnet50, inception_v3, mobilenet, xception, vgg16, vgg19
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
+
 import numpy as np
 import os
 # ERROR Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-import sys
-
-# get wsgi server to replace flask app.run()
-from gevent.pywsgi import WSGIServer
 # file processing
 from werkzeug.utils import secure_filename
 
+# import global variables (model_mobilenet, model_resnet50, model_inception_v3)
+import mymodels
+
+# create Flask app
 app = Flask(__name__)
 
+def predict_image(image_input, model) -> str:
+    tic = time()
+    print("predict_image() entered")
 
-model_mobilenet = load_model('models/MobileNet.h5')
-print("MobileNet model loaded.")
-
-model_resnet50 = load_model('models/ResNet50.h5')
-print("ResNet50 model loaded.")
-
-model_inception_v3 = load_model('models/InceptionV3.h5')
-print("InceptionV3 model loaded.")
-
-def predict_dog(image_input, model) -> str:
     if (model.name == 'mobilenet_1.00_224'):
-        model_preprocess_input = keras.applications.mobilenet.preprocess_input
-        model_decode_predictions = keras.applications.mobilenet.decode_predictions
+        #model.name = 'mobilenet'
+        model_preprocess_input = mobilenet.preprocess_input
+        model_decode_predictions = mobilenet.decode_predictions
     elif (model.name == 'resnet50'):
-        model_preprocess_input = keras.applications.resnet50.preprocess_input
-        model_decode_predictions = keras.applications.resnet50.decode_predictions
+        model_preprocess_input = resnet50.preprocess_input
+        model_decode_predictions = resnet50.decode_predictions
     elif (model.name == 'inception_v3'):
-        model_preprocess_input = keras.applications.inception_v3.preprocess_input
-        model_decode_predictions = keras.applications.inception_v3.decode_predictions
-    
+        model_preprocess_input = inception_v3.preprocess_input
+        model_decode_predictions = inception_v3.decode_predictions
+    elif (model.name == 'xception'):
+        model_preprocess_input = xception.preprocess_input
+        model_decode_predictions = xception.decode_predictions
+    elif (model.name == 'vgg16'):
+        model_preprocess_input = vgg16.preprocess_input
+        model_decode_predictions = vgg16.decode_predictions
+    elif (model.name == 'vgg19'):
+        model_preprocess_input = vgg19.preprocess_input
+        model_decode_predictions = vgg19.decode_predictions
+
     # read image input
     img = image.img_to_array(image_input)
-
     # reshape data for the model
     img = np.expand_dims(img, axis=0)
+    # prepare the image for the models
 
-    # prepare the image for the VGG model
-    img = model_preprocess_input(img)
+    if (model.name == 'resnet50'):
+        img = model_preprocess_input(img, mode='caffe')
+    else:
+        img = model_preprocess_input(img)
+
+    print("image preprocessed")
+    #pdb.set_trace()
 
     # predict the probability across all output classes
     predictions = model.predict(img)
 
+    print("image predicted")
+    #pdb.set_trace()
+
     # convert the probabilities to class labels
     label = model_decode_predictions(predictions, top=1)
 
+    print("predictions decoded")
+
     # retrieve the most likely result, e.g. highest probability
     label = label[0][0]
-
     # print the classification
-    classification = '%s predicts: %s (%.2f%%)' % (model.name, label[1], label[2]*100)
-    
-    #sys.stdout.write(classification)
-    
+    #classification = '%s predicts: %s (%.2f%%)' % (model.name, label[1], label[2]*100)
+
+    toc = time()
+
+    processing_time = toc-tic
+    print("Image classified by %s in >> %.2f s" % (model.name, processing_time))
+
+    classification = ( model.name, processing_time, label[2]*100, label[1] )
+    print("image classified >> " + str(classification))
+
     return (classification)
 
-    # show dog image
-    #plt.figure()
-    #plt.imshow(image)
 
 def get_file_path_and_save(request):
     # Get the file from post request
@@ -86,42 +108,45 @@ def get_file_path_and_save(request):
 
 @app.route('/', methods = ['GET'])
 def show_index():
+    print('entered homepage')
     return render_template('index.html')
 
-@app.route('/predictResNet50', methods = ['GET', 'POST'])
-def predict_image():
+@app.route('/predict', methods = ['GET', 'POST'])
+def generate_predictions():
+    print('entered predict image')
+
+    def row_prediction(image_input, model):
+        model_name, ptime, confidence, label = predict_image(image_input, model)
+        return '<tr><td> %s </td><td> %.2fs </td><td> %.2f%% </td><td> %s </td></tr>' % (model_name, ptime, confidence, label)
+
     if request.method == 'POST':
+        tic = time()
         file_path = get_file_path_and_save(request)
         img_224 = image.load_img(file_path, target_size = (224,224))
         img_299 = image.load_img(file_path, target_size = (299,299))
+
+        result = '<table style="width:70%">'
+        result += '<caption>Image Classifications for %s </caption>' % (os.path.basename(file_path))
+        result += '<tr><th>Machine Learning Model</th><th>Time</th><th>Confidence</th><th>Classification</th></tr>'
+
+
+        result += row_prediction(img_224, mymodels.model_vgg16)
         
-        result = predict_dog(img_224, model_mobilenet)
-        result += '\n'
-        result += predict_dog(img_224, model_resnet50)
-        result += '\n'
-        result += predict_dog(img_299, model_inception_v3)
+        result += row_prediction(img_224, mymodels.model_vgg19)
+
+        result += row_prediction(img_224, mymodels.model_resnet50)
+
+        result += row_prediction(img_299, mymodels.model_inception_v3)
+
+        result += row_prediction(img_299, mymodels.model_xception)
         
-        return result
-    return None
+        result += row_prediction(img_224, mymodels.model_mobilenet)
+        
 
-def predictInceptionV3():
-    if request.method == 'POST':
-        file_path = get_file_path_and_save(request)
+        result += '</table>'
 
-        img = image.load_img(file_path, target_size = (299,299))
-
-        result = predict_dog(img, model)
-
-        return result
-    return None
-
-def predictMobileNet():
-    if request.method == 'POST':
-        file_path = get_file_path_and_save(request)
-
-        img = image.load_img(file_path, target_size = (224,224))
-
-        result = predict_dog(img, model)
+        toc = time()
+        print("Total time for all predictions >> %.2f s" % (toc-tic) )
 
         return result
     return None
@@ -129,9 +154,11 @@ def predictMobileNet():
 if (__name__ == '__main__'):
     print('* Loading Keras models and starting Flask server...')
     print('.....')
-    # app.run()
+    # get wsgi server to replace flask app.run()
+    from gevent.pywsgi import WSGIServer
     web_server = WSGIServer(('', 5000), app)
     web_server.serve_forever()
 
+    print('Success! Server available at http://127.0.0.1:5000')
 
 
