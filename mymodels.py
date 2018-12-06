86f4a2c23ba9163eea88255080f8cfff10f78796
Chaehan So <chaehan.so@gmail.com>
2018-12-07 00:34:56 +0900

    Cleanup model_data DDict

--- mymodels.py
+++ mymodels.py
@@ -11,50 +11,69 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 Tic = time()
 
 model_data = {
-
-    'VGG16': {'image_size' : 224,'top1_acc' : 71.5, 'top5_acc' : 90.1,
-              'model_preprocess_input': vgg16.preprocess_input,
-              'model_decode_predictions': vgg16.decode_predictions
-               },
-    'VGG19': {'image_size' : 224, 'top1_acc' : 72.7, 'top5_acc' : 91.0,
-              'model_preprocess_input': vgg19.preprocess_input,
-              'model_decode_predictions': vgg19.decode_predictions
-              },
-    'ResNet50': {'image_size' : 224, 'top1_acc' : 75.9, 'top5_acc' : 92.9,
-                 'model_preprocess_input': resnet50.preprocess_input,
-                 'model_decode_predictions': resnet50.decode_predictions
-                 },
-    'InceptionV3': {'image_size' : 299, 'top1_acc' : 78.8, 'top5_acc' : 94.4,
-                    'model_preprocess_input': inception_v3.preprocess_input,
-                    'model_decode_predictions': inception_v3.decode_predictions
-                    },
-    'Xception': {'image_size' : 299, 'top1_acc' : 79.0, 'top5_acc' : 94.5,
-                 'model_preprocess_input': xception.preprocess_input,
-                 'model_decode_predictions': xception.decode_predictions
-                 },
+    'VGG16': {
+        'gs_bucket_label' : 'gc_vgg16',
+        'image_size' : 224,'top1_acc' : 71.5, 'top5_acc' : 90.1,
+        'model_preprocess_input': vgg16.preprocess_input,
+        'model_decode_predictions': vgg16.decode_predictions
+    },
+    'VGG19': {
+        'gs_bucket_label' : 'gc_vgg19',
+        'image_size' : 224, 'top1_acc' : 72.7, 'top5_acc' : 91.0,
+        'model_preprocess_input': vgg19.preprocess_input,
+        'model_decode_predictions': vgg19.decode_predictions
+    },
+    'ResNet50': {
+        'gs_bucket_label' : 'gc_resnet50',
+        'image_size' : 224, 'top1_acc' : 75.9, 'top5_acc' : 92.9,
+        'model_preprocess_input': resnet50.preprocess_input,
+        'model_decode_predictions': resnet50.decode_predictions
+    },
+    'InceptionV3': {
+        'gs_bucket_label' : 'gc_inception_v3',
+        'image_size' : 299, 'top1_acc' : 78.8, 'top5_acc' : 94.4,
+        'model_preprocess_input': inception_v3.preprocess_input,
+        'model_decode_predictions': inception_v3.decode_predictions
+    },
+    'Xception': {
+        'gs_bucket_label' : 'gc_xception',
+        'image_size' : 299, 'top1_acc' : 79.0, 'top5_acc' : 94.5,
+        'model_preprocess_input': xception.preprocess_input,
+        'model_decode_predictions': xception.decode_predictions
+    },
     # MobileNet Source: https://arxiv.org/pdf/1704.04861.pdf
-    'MobileNet': {'image_size' : 224, 'top1_acc' : 70.6, 'top5_acc' : 87.1,
-                  'model_preprocess_input': mobilenet.preprocess_input,
-                  'model_decode_predictions': mobilenet.decode_predictions
-                  },
+    'MobileNet': {
+        'gs_bucket_label' : 'gc_mobilenet',
+        'image_size' : 224, 'top1_acc' : 70.6, 'top5_acc' : 87.1,
+        'model_preprocess_input': mobilenet.preprocess_input,
+        'model_decode_predictions': mobilenet.decode_predictions
+    },
     # MobileNetV2 Source: https://github.com/tensorflow/models/tree/master/research/slim/nets/mobilenet
-    'MobileNetV2': {'image_size' : 224, 'top1_acc' : 71.8, 'top5_acc' : 91.0,
-                    'model_preprocess_input': mobilenet_v2.preprocess_input,
-                    'model_decode_predictions': mobilenet_v2.decode_predictions
-                    },
+    'MobileNetV2': {
+        'gs_bucket_label' : 'gc_mobilenet_v2',
+        'image_size' : 224, 'top1_acc' : 71.8, 'top5_acc' : 91.0,
+        'model_preprocess_input': mobilenet_v2.preprocess_input,
+        'model_decode_predictions': mobilenet_v2.decode_predictions
+    },
     #(NasNetMobile Source: https://github.com/tensorflow/models/tree/master/research/slim/nets/nasnet)
-    'NASNetMobile': {'image_size' : 224, 'top1_acc' : 74.0, 'top5_acc' : 91.6,
-                     'model_preprocess_input': nasnet.preprocess_input,
-                     'model_decode_predictions': nasnet.decode_predictions
-                     },
-    'DenseNet201': {'image_size' : 224, 'top1_acc' : 77.0, 'top5_acc' : 93.3,
-                    'model_preprocess_input': densenet.preprocess_input,
-                    'model_decode_predictions': densenet.decode_predictions
-                    },
-    'InceptionResNetV2': {'image_size' : 299, 'top1_acc' : 80.1, 'top5_acc' : 95.1,
-                          'model_preprocess_input': inception_resnet_v2.preprocess_input,
-                          'model_decode_predictions': inception_resnet_v2.decode_predictions
-                         }
+    'NASNetMobile': {
+        'gs_bucket_label' : 'gc_nasnet_mobile',
+        'image_size' : 224, 'top1_acc' : 74.0, 'top5_acc' : 91.6,
+        'model_preprocess_input': nasnet.preprocess_input,
+        'model_decode_predictions': nasnet.decode_predictions
+    },
+    'DenseNet201': {
+        'gs_bucket_label' : 'densenet201',
+        'image_size' : 224, 'top1_acc' : 77.0, 'top5_acc' : 93.3,
+        'model_preprocess_input': densenet.preprocess_input,
+        'model_decode_predictions': densenet.decode_predictions
+    },
+    'InceptionResNetV2': {
+        'gs_bucket_label' : 'inception_resnet_v2',
+        'image_size' : 299, 'top1_acc' : 80.1, 'top5_acc' : 95.1,
+        'model_preprocess_input': inception_resnet_v2.preprocess_input,
+        'model_decode_predictions': inception_resnet_v2.decode_predictions
+    }
 }
 
 def load_keras_model(model_label):
